e13673d0c6660e50f5b8286563664a38b81e9647
JV13 <cherkas.ivan13@gmail.com>
2025-07-25 16:00:59 +0300

    added initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [AuthModule, ProductModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
