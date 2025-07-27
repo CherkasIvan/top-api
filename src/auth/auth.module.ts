d2420cfa6b1a46991fc3cbdbf8e3b09d70f9d2df
JV13 <cherkas.ivan13@gmail.com>
2025-07-27 12:06:39 +0300

    commit resolving dn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,14 @@
+import { TypegooseModule } from 'nestjs-typegoose';
+
 import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
+import { AuthModel } from './auth.model';
 import { AuthService } from './service/auth.service';
 
 @Module({
     providers: [AuthService],
     controllers: [AuthController],
+    imports: [TypegooseModule.forFeature([AuthModel])],
 })
 export class AuthModule {}
