b3a73c166f5da795f1aae9e23c58be0ec9e715e7
JV13 <cherkas.ivan13@gmail.com>
2025-07-27 11:51:14 +0300

    Conected mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
+import { TypegooseModule } from 'nestjs-typegoose';
+
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { getMongoConfig } from './configs/mongo.config';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
         AuthModule,
         ProductModule,
         ReviewModule,
